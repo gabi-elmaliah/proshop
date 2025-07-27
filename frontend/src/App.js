da388af25c1bbc2230b457e58f1e7c048d46318f
gabi-elmaliah <gabbyelmaliah@gmail.com>
2025-07-27 12:45:02 +0100

    first version of frontend completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';  
+import {Outlet} from 'react-router-dom'
 import React from 'react'
 
 const App = () => {
@@ -9,7 +11,8 @@ const App = () => {
       <Header />
       <main className='py-3' >
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <Outlet />
+      
         </Container>
       </main>
       <Footer />
